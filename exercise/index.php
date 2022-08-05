c87d9ba6c4d8aab9ef207b1a9dd46fab5900c00c
labandiroma <labandiromarygrace@gmail.com>
2022-08-05 14:19:15 +0800

    Added arguments conditions

--- exercise/index.php
+++ exercise/index.php
@@ -4,8 +4,7 @@
 	//argv => contains the value of the argument
 	//trim => remove whitespace/tabs
 	//array_fill => fills and array with value ; start_index -> first index; count => number of elements to be inserter; value -> value
-
-	$argumentsData = array_fill(0, 6, NULL);
+	//isset => checks if variable is set, it has to be declared and is not NULL
 
 	$fileName = $argv[0];
 	$personCode = $argv[1];
@@ -13,13 +12,13 @@
 	$familyName = $argv[3];
 	$gender = $argv[4];
 	$birthDate = $argv[5];
- 
+
 	if($argc == 1 || $argv[1] == "--help")
 	{
 		echo "\nExample Command:\n";
 		echo "php <file  name> <person code> <first name> <family name> <gender> <birth date>\n";
 	}
-	else
+	else if ($argc == 7)
 	{
 		$printData =  "php" . " " . $fileName . " " . $personCode . " " . $firstName . " " . $familyName . " " . $gender . " " . $birthDate;
 
@@ -28,5 +27,9 @@
 		$personRecords = fopen("personRecords.txt", "a");
 		fwrite($personRecords, $printData . "\n");
 		fclose($personRecords);
+	}
+	else
+	{
+		echo "Error! Follow correct arguments";
 	}
 ?>
