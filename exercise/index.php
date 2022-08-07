d57f5f101f7abd44c25204ea463d26f85edf85b9
labandiroma <labandiromarygrace@gmail.com>
2022-08-07 22:46:38 +0800

    Added function for output

--- exercise/index.php
+++ exercise/index.php
@@ -5,6 +5,7 @@
 	//trim => remove whitespace/tabs
 	//array_fill => fills and array with value ; start_index -> first index; count => number of elements to be inserter; value -> value
 	//isset => checks if variable is set, it has to be declared and is not NULL
+	//what inside the system?? Input and Output, process kay in-between
 
 	$fileName = $argv[0];
 	$personCode = $argv[1];
@@ -13,12 +14,21 @@
 	$gender = $argv[4];
 	$birthDate = $argv[5];
 
-	if($argc == 1 || $argv[1] == "--help")
+	if($argc == 2 && $argv[1] == "--help")
 	{
 		echo "\nExample Command:\n";
 		echo "php <file  name> <person code> <first name> <family name> <gender> <birth date>\n";
 	}
 	else if ($argc == 6)
+	{
+		printDatatoFile($fileName, $personCode, $firstName, $familyName, $gender, $birthDate);
+	}
+	else
+	{
+		echo "Error! Follow correct arguments";
+	}
+
+	function printDatatoFile($fileName, $personCode, $firstName, $familyName, $gender, $birthDate)
 	{
 		$printData =  "php" . " " . $fileName . " " . $personCode . " " . $firstName . " " . $familyName . " " . $gender . " " . $birthDate;
 
@@ -27,9 +37,5 @@
 		$personRecords = fopen("personRecords.txt", "a");
 		fwrite($personRecords, $printData . "\n");
 		fclose($personRecords);
-	}
-	else
-	{
-		echo "Error! Follow correct arguments";
 	}
 ?>
